f3185526e12bb876875a71397be15e5d4eb25965
tnaums <tanaumann@outlook.com>
2026-02-01 16:08:56 -0600

    ch1 l5 Servers Fileservers

--- main.go
+++ main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	//	"time"
 	"log"
 )
 
 func main() {
-	fmt.Println("Welcome to chirpy")
 	const port = "8080"
 
 	// use the http.NewServerMux() function to create an empty servemux
 	mux := http.NewServeMux()
 
+	// Use the http.FileServer() function to create a handler
+	fh := http.FileServer(http.Dir("."))
+	rh := http.RedirectHandler("http://example.org", 307)	
+	mux.Handle("/", fh)
+	mux.Handle("/foo", rh)
+	
 	s := &http.Server{
 	Addr:           ":" + port,
 	Handler:        mux,
 	}
+	
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(s.ListenAndServe())	
 
