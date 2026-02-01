0791743da120ec40d0e0e5ef118ce22c3b3942a9
tnaums <tanaumann@outlook.com>
2026-02-01 15:37:40 -0600

    ch1 l1 Servers Server

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	//	"time"
+	"log"
+)
 
 func main() {
 	fmt.Println("Welcome to chirpy")
+	const port = "8080"
+
+	// use the http.NewServerMux() function to create an empty servemux
+	mux := http.NewServeMux()
+
+	s := &http.Server{
+	Addr:           ":" + port,
+	Handler:        mux,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(s.ListenAndServe())	
+
 }
