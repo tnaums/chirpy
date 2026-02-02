5969b8b5b63d5096ac3bc9feccda5651686f8bcc
tnaums <tanaumann@outlook.com>
2026-02-02 01:01:49 -0600

    finished chapter 1 Servers

--- main.go
+++ main.go
@@ -3,26 +3,53 @@ package main
 import (
 	"net/http"
 	"log"
+	"time"
 )
+
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello, world!\n"))
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	tm := time.Now().Format(time.RFC1123)
+	w.Write([]byte("The time is: " + tm + "\n"))
+}
+
+func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK\n"))
+}
 
 func main() {
 	const port = "8080"
-
+	const filepathRoot = "."
 	// use the http.NewServerMux() function to create an empty servemux
 	mux := http.NewServeMux()
 
+
+	// Initialise the timeHandler in exactly the same way we would any normal
+	// struct.
+	th := http.HandlerFunc(timeHandler)
+	re := http.HandlerFunc(readinessEndpoint)
+	hw := http.HandlerFunc(helloHandler)
+
 	// Use the http.FileServer() function to create a handler
-	fh := http.FileServer(http.Dir("."))
+	//	fs := http.FileServer(http.Dir(filepathRoot))
 	rh := http.RedirectHandler("http://example.org", 307)	
-	mux.Handle("/", fh)
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
 	mux.Handle("/foo", rh)
+	mux.Handle("/time", th)	
+	mux.Handle("/healthz", re)
+	mux.Handle("/hello", hw)
 	
 	s := &http.Server{
 	Addr:           ":" + port,
 	Handler:        mux,
 	}
-	
-	log.Printf("Serving on port: %s\n", port)
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)	
 	log.Fatal(s.ListenAndServe())	
 
 }
